cd10bc4e75372abddafd40051e07d697e2d42529
Karl Martin Sola <kms@tasta.no>
2018-02-25 16:31:06 +0100

    Update vm-bootstrap.sh

--- vm-bootstrap.sh
+++ vm-bootstrap.sh
@@ -10,5 +10,9 @@ sudo systemctl enable docker
 sudo yum install -y java-1.8.0-openjdk-headless epel-release
 sudo yum install -y jq git python2-pip
 sudo pip install csvtojson
+sudo pip install docker-compose
 
 sudo systemctl start docker
+
+sudo su - centos -c "git clone -b x-pack https://github.com/deviantony/docker-elk.git"
+sudo su - centos "cd docker-elk && docker-compose up -d"
